38abd58fbf1663bf3d7b929f3c1e7002e60b5d37
Lukman <lukmanmakhaev@gmail.com>
2023-01-04 22:40:27 +0300

    added likes, location etc

--- UnsplashApp/Views/ExploreView/DetailsViewController.swift
+++ UnsplashApp/Views/ExploreView/DetailsViewController.swift
@@ -24,6 +24,17 @@ let locationLabel: UILabel = {
     return label
 }()
 
+let createdAtLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+}()
+
+let likesLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+}()
 
 class DetailsViewController: UIViewController {
     
@@ -43,11 +54,23 @@ class DetailsViewController: UIViewController {
         locationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         locationLabel.topAnchor.constraint(equalTo: pic.bottomAnchor).isActive = true
+        
+        view.addSubview(createdAtLabel)
+        createdAtLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        createdAtLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        createdAtLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor).isActive = true
+        
+        view.addSubview(likesLabel)
+        likesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        likesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        likesLabel.topAnchor.constraint(equalTo: createdAtLabel.bottomAnchor).isActive = true
 
     }
     
-    func updateData(title: String, picture: String, location: String) {
-        locationLabel.text = location
+    func updateData(title: String, picture: String, location: String, date: String, likes: String) {
+        likesLabel.text = "Likes: \(likes)"
+        locationLabel.text = "Location: \(location)"
+        createdAtLabel.text = "Created in: \(date)"
         picUrl = picture
         self.title = title
         pic.kf.setImage(with: URL(string: picUrl))
